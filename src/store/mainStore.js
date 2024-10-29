1443b7086a04dd5f8d6e6b50cbdc5aebb45953f8
MincheolSin <smc970417@gmail.com>
2024-10-29 17:12:16 +0900

    first

---
+++ src/store/mainStore.js
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia';
+import { reactive } from 'vue';
+
+export const useMainStore = defineStore('mainStore',()=>{
+    
+    const state = reactive({
+        
+    });
+
+
+    return {
+      
+    }
+},
+);
