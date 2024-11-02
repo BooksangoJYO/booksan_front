73a50234b76f8fffeded4a44743b55f90a783420
MincheolSin <smc970417@gmail.com>
2024-11-02 10:12:38 +0900

    chat page

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,33 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
+<<<<<<< Updated upstream
         target: 'http://localhost:8090',
         changeOrigin: true,
         secure:false,
       },
     },
+=======
+        target: 'http://192.168.0.21:8190', // 요청을 보낼 프록시 서버 URL
+      },
+      '/ws-stomp': {
+        target: 'http://192.168.0.21:8190',
+        ws: true
+      },
+      '/pub': {
+        target: 'http://192.168.0.21:8190',
+        ws: true
+      },
+      '/sub': {
+        target: 'http://192.168.0.21:8190',
+        ws: true
+      }
+    },
+    
+  },
+
+  define: {
+    global: 'window'
+>>>>>>> Stashed changes
   },
 })
