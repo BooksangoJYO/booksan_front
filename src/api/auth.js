a9f6051ee22561f656cedaf47feef34a5e1a3660
ggwag <gdh6356@gmail.com>
2024-11-11 16:40:23 +0900

    Feat: 카카오 로그인, 로그아웃, 회원가입,마이페이지  테스트 뷰 구성

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,17 +1,41 @@
 // auth.js
-import apiClient from './api.js';
+import api from './api.js';
 
 // 멤버 로그인
 function login(uid, email) {
-    return apiClient.post('/api/users/auth/socialLogin', { uid, email });
+    return api.apiClient.post('/users/auth/socialLogin', { uid, email });
+}
+
+// 멤버 회원가입
+function signup(email, uid, nickname) {
+    return api.apiClient.post('/users/signup', {uid, email, nickname});
 }
 
 // 멤버 로그아웃
 function logout() {
-    return apiClient.post('/api/users/logout'); 
+    return api.apiClient.post('/users/logout');
+}
+
+// 마이페이지 정보 조회
+function getUserInfo() {
+    return api.apiClient.get('/users/mypage');
+}
+
+// 프로필 정보 수정
+function updateProfile(userData) {
+    return api.apiClient.post('/users/update', userData);
+}
+
+// 회원 탈퇴
+function deleteAccount() {
+    return api.apiClient.delete('/users/delete');
 }
 
 export default {
     login,
     logout,
+    signup,
+    getUserInfo,
+    updateProfile,
+    deleteAccount,
 };
--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,26 @@ import Book from '@/components/board/BookSearch.vue';
 import ChatRoom from '@/components/ChatRoom.vue';
 import ChatRoomList from '@/components/ChatRoomList.vue';
 import { createRouter, createWebHistory } from 'vue-router';
-import SocialLogin from '../components/SocialLogin.vue';
+import SocialLogin from '@/components/user/SocialLogin.vue';
+import SocialSignup from '@/components/user/SocialSignup.vue';
+import LogoutTest from '@/components/user/LogoutTest.vue';
+import ProfileInfo from '@/components/user/mypage/ProfileInfo.vue';
+import Mypage from '@/components/user/mypage/index.vue';
+import KakaoCallback from '@/components/user/KakaoCallback.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         //{ path: '/', component: Main },
-        { path: '/login', component: SocialLogin  },
-        { path: '/book', component: Book},
-        {path:'/chat/roomList',component : ChatRoomList},
-        {path: '/chat/room/enter/:roomId',component: ChatRoom}
+        { path: '/login', name: 'login', component: SocialLogin },
+        { path: '/signup', name: 'signup', component: SocialSignup },
+        { path: '/logout', name: 'LogoutTest', component: LogoutTest },
+        { path: '/mypage', name: 'mypage', component: Mypage },
+        { path: '/profileInfo', name: 'profileInfo', component: ProfileInfo },
+        { path: '/auth/kakao/callback', name: 'KakaoCallback', component: KakaoCallback },
+        { path: '/book', component: Book },
+        { path:'/chat/roomList',component : ChatRoomList },
+        { path: '/chat/room/enter/:roomId',component: ChatRoom }
     ]
 });
 
