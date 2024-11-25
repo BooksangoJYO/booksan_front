a7252caad91caffc273f8b61cd774f41e2d1331d
김구 <skk3010g@gmail.com>
2024-11-25 10:30:33 +0900

    hotfix

--- src/components/board/BoardList.vue
+++ src/components/board/BoardList.vue
@@ -51,7 +51,7 @@
     <div v-if="filteredBoardList.length > 0" class="board-list">
       <div v-for="(board, index) in filteredBoardList" :key="index" class="board-item" @click="goToBoardRead(board.dealId)">        
         <div class="board-item-image">         
-          <img :src="board.imageFileListDTO[0] ? API_URLS.BOARD + '/api/board/read/download/' + board.imageFileListDTO[0].imgId : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKC5SbJx2Wf9ewguE1uvgE3zc5TRnX1XfOsA&s'" alt="Book Image" />
+          <img :src="board.imageFileListDTO.length ? API_URLS.BOARD + '/api/board/read/download/' + board.imageFileListDTO[0].imgId : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKC5SbJx2Wf9ewguE1uvgE3zc5TRnX1XfOsA&s'" alt="Book Image" />
         </div>
         <div class="board-item-details">
           <div class="board-item-info">
